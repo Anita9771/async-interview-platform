8d12395df9da795f3d19792a2f760b68bfce7620
Anita Eghonghon Idemudia <Ieghonghon@gmail.com>
2023-05-03 09:09:51 +0100

    latest

---
+++ Frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import { LandingPage, MainPage, SuccessPage } from './pages';
+
+function App() {
+  
+  return (
+   <Router>
+     <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/main-page" element={<MainPage />} />
+        <Route path="/success-page" element={<SuccessPage />} />
+      </Routes>
+    </div>
+   </Router>
+  );
+}
+
+export default App;
